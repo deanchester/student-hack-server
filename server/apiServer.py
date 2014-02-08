f9056212e8337960d733ccfe128d1eeae9461f1c
Dean Chester <dean@maquib.com>
2014-02-08 16:28:58 +0000

    Changed methods to return our structured data from autotraders API. :D

--- server/apiServer.py
+++ server/apiServer.py
@@ -15,10 +15,12 @@ def getAccessToken():
     return resp.read()
 
 def getFirstClassifiedAds(access_token):
+    """
+    This method returns the first advert.
+    """
     headers = {'Access-Token': access_token}
     req = requests.get(__access_classified_ads__, headers=headers)
-    reqJson = req.json()
-    print type(reqJson)
+    reqJson = convert(req.json())
     print reqJson['searchResults']['classifiedAdverts'][0]
 
 def getRandomAds(access_token, number_to_return):
@@ -26,17 +28,27 @@ def getRandomAds(access_token, number_to_return):
     This method returns number_to_return random ads as a dict
     """
     headers = {'Access-Token': access_token}
-    ads = {}
+    ads = []
     for i in range(0, number_to_return):
-        parameters = {"Page_Size": 100, "Page_Number": i}
+        parameters = {"Page_Size": 100, "Page_Number": randrange(100)}
         req = requests.get(__access_classified_ads__, params=parameters, headers=headers)
         reqJson = req.json()
         reqJson = convert(reqJson)
-        ads.update({str(i):reqJson['searchResults']['classifiedAdverts'][randrange(100)]})
-    return ads
+        randAd = randrange(100)
+        adTitle = reqJson['searchResults']['classifiedAdverts'][randAd]['advertAttributes']['advertTitle']
+        adDescription = reqJson['searchResults']['classifiedAdverts'][randAd]['advertAttributes']['description']
+        adPrice = reqJson['searchResults']['classifiedAdverts'][randAd]['advertAttributes']['price']
+        adSpecs = reqJson['searchResults']['classifiedAdverts'][randAd]['vehicleAttributes']
+        ad = {'title': adTitle, 'description': adDescription, 'price': adPrice, 'car_specs': adSpecs}
+        ads.append(ad)
+    adsChanged = {"cars": ads}
+    return adsChanged
 
 
 def convert(input):
+    """
+    This method converts the unicode dict to ascii.
+    """
     if isinstance(input, dict):
         return {convert(key): convert(value) for key, value in input.iteritems()}
     elif isinstance(input, list):
