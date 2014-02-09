f177bf4fdb61207331f5f13dfaa8bfa2885c30ba
Dean Chester <dean@maquib.com>
2014-02-09 08:06:37 +0000

    Moved to using tornado encoder.

--- server/Server.py
+++ server/Server.py
@@ -3,6 +3,7 @@ __email__ = 'dean.g.chester@gmail.com'
 
 import tornado.ioloop
 import tornado.web
+import tornado.escape
 import apiServer
 import json
 
@@ -15,22 +16,24 @@ class HandleComms(tornado.web.RequestHandler):
     def get(self):
         accessToken = apiServer.getAccessToken()
         randomAdverts = apiServer.getRandomAds(accessToken, 8)
-        self.write(json.dumps(randomAdverts))
+        self.write(tornado.escape.json_encode(randomAdverts))
         self.set_cookie(__str_cookie_name__, str(1))
 
 
     def post(self):
         iteriation = self.get_cookie(__str_cookie_name__)
         accessToken = apiServer.getAccessToken()
+
+        print self.request.body
         car = tornado.escape.json_decode(self.request.body)
         car = apiServer.convert(car)
         carsSelected.append(car)
         if(len(carsSelected) >= 3):
             nextAdverts = apiServer.getUpdatedAds(accessToken, carsSelected, 8)
-            self.write(json.dumps(nextAdverts))
+            self.write(tornado.escape.json_encode(nextAdverts))
         else:
             randomAdverts = apiServer.getRandomAds(accessToken, 8)
-            self.write(json.dumps(randomAdverts))
+            self.write(tornado.escape.json_encode(randomAdverts))
             self.set_cookie(__str_cookie_name__, str(len(carsSelected)))
 
 
@@ -39,5 +42,5 @@ if __name__ == "__main__":
     application = tornado.web.Application([
         (r"/cars", HandleComms),
     ])
-    application.listen(8888)
+    application.listen(80)
     tornado.ioloop.IOLoop.instance().start()
