81fc49759d5fb28cc6ae59276addc67313595a53
Dean Chester <dean@maquib.com>
2014-02-09 07:03:11 +0000

    Changed to tornado json shit.

--- server/Server.py
+++ server/Server.py
@@ -22,7 +22,7 @@ class HandleComms(tornado.web.RequestHandler):
     def post(self):
         iteriation = self.get_cookie(__str_cookie_name__)
         accessToken = apiServer.getAccessToken()
-        car = json.loads(self.request.body)
+        car = tornado.escape.json_decode(self.request.body)
         car = apiServer.convert(car)
         carsSelected.append(car)
         if(len(carsSelected) >= 3):
