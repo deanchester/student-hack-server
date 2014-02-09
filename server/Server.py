5357ebbdc9ee9d4b6927ee1001aaa0dabf2c16c7
Dean Chester <dean@maquib.com>
2014-02-09 09:51:32 +0000

    Fixed security,

--- server/Server.py
+++ server/Server.py
@@ -10,9 +10,6 @@ import json
 __str_cookie_name__ = "cars"
 
 class HandleComms(tornado.web.RequestHandler):
-    def set_default_headers(self):
-        self.set_header("Access-Control-Allow-Origin", "*")
-
     def get(self):
         accessToken = apiServer.getAccessToken()
         randomAdverts = apiServer.getRandomAds(accessToken, 8)
